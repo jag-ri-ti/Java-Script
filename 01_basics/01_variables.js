14d401427f7d7a61cc4a639f3bf430d7d70b1b8e
Jagriti <jagritip081@gmail.com>
2024-11-30 13:22:48 +0530

    JS basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = '144553'  //can't change
+let accountEmail = "Jango@gmail.com"  //can change
+var accountPassword = "12345"// can change
+accountCity = "Pune"  //can change
+let accountstate;
+
+//accountId=2   //not allowed
+
+accountEmail="hjhhj@gmail.com"
+accountPassword="76543" 
+accountCity="Delhi"
+ 
+console.log(accountCity)
+console.log(accountEmail)
+console.log(accountPassword)
+
+
+//OR
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
+
+/*
+prefer not to use var in code
+*/
+//END
